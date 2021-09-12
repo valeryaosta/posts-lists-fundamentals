95bf8eb70c74f6a4ca819201b91bdf7480618874
valeryaosta <gerasimenko.valery@gmail.com>
2021-09-12 22:43:59 +0300

    state changes (class+func components)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ClassCounter from "./components/ClassCounter";
 
 
 function App() {
+
     return (
-        <div className="App">
-            Start!
+        <div className='App'>
+            <Counter/>
+            <ClassCounter/>
+
         </div>
     );
 }
