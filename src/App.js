fa868e9507ee8cf0738da529247e5f67fc1c80ca
valeryaosta <gerasimenko.valery@gmail.com>
2021-09-16 23:59:15 +0300

    filter / sort

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React, {useState} from "react";
+import React, {useMemo, useState} from "react";
 import "./styles/App.css";
 import PostList from "./components/UI/PostList";
 import PostForm from "./components/UI/PostForm";
-import MySelect from "./components/UI/select/MySelect";
+import PostFilter from "./components/PostFilter";
 
 
 function App() {
 
     const [posts, setPosts] = useState([
-        {id: 1, title: 'React', body: 'UI-library'},
-        {id: 2, title: 'JavaScript', body: 'Native language of JS'},
-        {id: 3, title: 'Redux', body: 'State management'}
+        {id: 1, title: "React", body: "UI-library"},
+        {id: 2, title: "JavaScript", body: "Native language of JS"},
+        {id: 3, title: "Redux", body: "State management"}
     ]);
 
-    const [selectedSort, setSelectedSort] = useState('');
+    const [filter, setFilter] = useState({sort: "", query: ""});
+
+    const sortedPosts = useMemo(() => {
+        console.log("function called");
+        if (filter.sort) {
+            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
+        }
+        return posts;
+    }, [filter.sort, posts]);
+
+    const sortedAndSearchedPosts = useMemo(() => {
+        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
+    }, [filter.query, sortedPosts]);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
@@ -23,29 +35,15 @@ function App() {
         setPosts(posts.filter(p => p.id !== post.id));
     };
 
-    const sortPosts = (sort) => {
-        setSelectedSort(sort);
-        setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
-    };
-
     return (
         <div className="App">
             <PostForm create={createPost}/>
-            <hr style={{margin: '15px 0'}}/>
-            <div>
-                <MySelect value={selectedSort}
-                          onChange={sortPosts}
-                          defaultValue="Сортировка"
-                          options={[
-                              {value: 'title', name: 'По названию'},
-                              {value: 'body', name: 'По описанию'}
-                          ]}
-                />
-            </div>
-            {posts.length !== 0
-                ? <PostList posts={posts} title={'Список постов JS'} remove={removePost}/>
-                : <h1 style={{textAlign: 'center'}}>Посты не найдены</h1>
-            }
+            <hr style={{margin: "15px 0"}}/>
+            <PostFilter filter={filter}
+                        setFilter={setFilter}
+            />
+            <PostList posts={sortedAndSearchedPosts} title={"Список постов JS"} remove={removePost}/>
+
         </div>
     );
 }
