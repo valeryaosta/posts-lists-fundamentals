6851f0bbcc48c5dc18a7a3a9a8294836029873ed
valeryaosta <gerasimenko.valery@gmail.com>
2021-09-17 20:42:05 +0300

    get posts from API with axios

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useMemo, useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./styles/App.css";
 import PostList from "./components/UI/PostList";
 import PostForm from "./components/UI/PostForm";
@@ -6,22 +6,29 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
-
+import axios from "axios";
+import {PostService} from "./API/PostService";
 
 function App() {
 
     const [posts, setPosts] = useState([]);
-
     const [filter, setFilter] = useState({sort: "", query: ""});
-
     const [modal, setModal] = useState(false);
-
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
+
+    useEffect(() => {
+        fetchPosts();
+    }, []);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
         setModal(false);
     };
+
+    async function fetchPosts() {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    }
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id));
@@ -29,6 +36,7 @@ function App() {
 
     return (
         <div className="App">
+            <button onClick={fetchPosts}>GET POSTS</button>
             <MyButton onClick={() => setModal(true)} style={{marginTop: 30}}>
                 Создать пользователя
             </MyButton>
