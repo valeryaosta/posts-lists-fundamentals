7bdc1267184e3528d9f5c3060822f18a1b3e27d6
valeryaosta <gerasimenko.valery@gmail.com>
2021-09-17 19:52:19 +0300

    custom hooks for sorting

--- src/App.js
+++ src/App.js
@@ -5,30 +5,18 @@ import PostForm from "./components/UI/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/modal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 
 function App() {
 
-    const [posts, setPosts] = useState([
-        {id: 1, title: "React", body: "UI-library"},
-        {id: 2, title: "JavaScript", body: "Native language of JS"},
-        {id: 3, title: "Redux", body: "State management"}
-    ]);
+    const [posts, setPosts] = useState([]);
 
     const [filter, setFilter] = useState({sort: "", query: ""});
 
     const [modal, setModal] = useState(false);
 
-    const sortedPosts = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]));
-        }
-        return posts;
-    }, [filter.sort, posts]);
-
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query));
-    }, [filter.query, sortedPosts]);
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost]);
